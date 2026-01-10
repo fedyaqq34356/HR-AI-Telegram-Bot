80df70a9db47a5e2248e87166c500a6b42e2369e
fedya <fedya.3.4.6.2.7@gmail.com>
2026-01-10 22:33:38 +0200

    add __init__.py

---
+++ keyboards/__init__.py
@@ -0,0 +1,21 @@
+from .user import groups_keyboard
+from .admin import (
+    admin_review_keyboard,
+    admin_answer_keyboard,
+    admin_panel_keyboard,
+    admin_main_menu,
+    forbidden_topics_keyboard,
+    users_list_keyboard,
+    conversation_keyboard
+)
+
+__all__ = [
+    'groups_keyboard',
+    'admin_review_keyboard',
+    'admin_answer_keyboard',
+    'admin_panel_keyboard',
+    'admin_main_menu',
+    'forbidden_topics_keyboard',
+    'users_list_keyboard',
+    'conversation_keyboard'
+]
