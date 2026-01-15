3dd8281c560eeaa2811e9b4b69af70025910e04f
Fedya <fedya.3.4.6.2.7@gmail.com>
2026-01-16 01:39:03 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,21 +1,21 @@
-from .user import groups_keyboard
 from .admin import (
     admin_review_keyboard,
     admin_answer_keyboard,
-    admin_panel_keyboard,
     admin_main_menu,
+    admin_panel_keyboard,
     forbidden_topics_keyboard,
     users_list_keyboard,
-    conversation_keyboard
+    conversation_keyboard,
+    cancel_keyboard
 )
 
 __all__ = [
-    'groups_keyboard',
     'admin_review_keyboard',
     'admin_answer_keyboard',
-    'admin_panel_keyboard',
     'admin_main_menu',
+    'admin_panel_keyboard',
     'forbidden_topics_keyboard',
     'users_list_keyboard',
-    'conversation_keyboard'
+    'conversation_keyboard',
+    'cancel_keyboard'
 ]
